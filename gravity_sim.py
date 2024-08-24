7ea2672a2f62fb520565853c2b863185b60c01d6
Parthiv <parthivshetty1@gmail.com>
2024-08-25 00:54:54 +0530

    added features

--- gravity_sim.py
+++ gravity_sim.py
@@ -1,27 +1,17 @@
 import pygame
 import math
 import random
+
+
+
+
+n = int(input("enter no of planets"))
 pygame.init()
+
 
 screen = pygame.display.set_mode((800, 600))
 black = (0, 0, 0)
 running = True
-
-'''
-x1, y1, m1 ,vx1, vy1 = 100, 400, 1, 0, 0
-
-x2, y2, m2 ,vx2, vy2= 400, 100, 1,0 ,0
-
-x3, y3, m3 ,vx3, vy3= 700, 400, 1,0 ,0
-
-
-
-centre = [400,300, 5, 0, 0, (0,0,0)]
-body1 = [x1, y1, m1 ,vx1, vy1, (66, 135, 245)]
-body2 =[x2, y2, m2 ,vx2, vy2,(245, 66, 87)]
-body3 =[x3, y3, m3 ,vx3, vy3,(245, 191, 66)]
-
-bodies = [centre,body1,body2,body3]'''
 
 
 def initialize(ls,n):
@@ -35,11 +25,20 @@ def initialize(ls,n):
         temp = [x, y, m, vx, vy, color]
         ls.append(temp)
     return ls
+    
 G = 0.5
 
 bodies= []
-bodies = initialize(bodies,5)
-def update(ls):
+
+bodies = initialize(bodies,n)
+
+history = []
+for i in range(n):
+    history.append([])
+
+
+
+def update(ls,mode=0):
     force= []
     for i in range(len(ls)):
         force.append((0,0))
@@ -77,8 +76,40 @@ def update(ls):
         x  += vx
         y  += vy
         ls[i] = x, y, m, vx, vy, c 
+
+        ##trace stuff
+        if mode == 0:
+            if len(history[i]) >5000:
+                history[i].pop(0)    
+            history[i].append((x,y))
     return ls
 
+def track(bodies):
+    xmean = 0
+    ymean = 0
+    for i in bodies:
+        x,y,_,_,_,_ = i
+        xmean += x
+        ymean += y
+    xmean /= len(bodies)
+    ymean /= len(bodies)
+
+    xmean -= 400
+    ymean -=300
+    for i in range(len(bodies)):
+        x,y,i1,i2,i3,i4 = bodies[i]
+        x -= xmean
+        y -= ymean
+        bodies[i] = x,y,i1,i2,i3,i4
+    return bodies
+
+
+def trace(ls):
+
+    for i in range(len(history)):
+        for j in range(len(history[i])-1):
+            _,_,_,_,_,c = bodies[i]
+            pygame.draw.aaline(screen,c,history[i][j+1],history[i][j])
         
 def draw(ls):
 
@@ -86,17 +117,38 @@ def draw(ls):
         x,y,m,vx,vy,c = i
         pygame.draw.circle(screen, c, (int(x), int(y)), m)
 
+def gravity_well(bodies,mouse_pos):
+    x,y = mouse_pos
+    print(x,y)
+    well = [x,y,9999999999999,0,0,(0,0,0)]
+    ls = bodies.copy()
+    ls.append(well)
+    ls = update(ls,1)
+    return ls[:-1]
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+    
+    mouse_buttons = pygame.mouse.get_pressed()
+    mouse_pos = pygame.mouse.get_pos()
 
-    screen.fill(black)
-    bodies = update(bodies)
-    draw(bodies)
+    if mouse_buttons[0]:  # Left mouse button is held down
+        if not gravity_well_active:
+            bodies = gravity_well(bodies, mouse_pos)
+            gravity_well_active = True
+    else:  # Mouse button is not pressed
+        gravity_well_active = False
 
     
- 
+    screen.fill(black)
+    bodies = track(bodies)
+
+    bodies = update(bodies)
+    trace(bodies)
+    
+    draw(bodies)
+    
     
     pygame.display.flip()
 
